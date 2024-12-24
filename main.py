338d8a66c04cb6394964b3de94af4804325fd956
George Steil <steilga92@gmail.com>
2024-12-24 10:10:57 -0600

    Started asteroids project over again. Added files main.py, requirements.txt and directory venv for a virtual environment

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Project Name: Asteroids with Python
+Creation Date: 12/24/2024
+Creator Name: George Steil
+Modified Date:
+"""
+
+import pygame
+
+def main():
+	print("Starting asteroids!")
+
+if __name__ == "__main__":
+	main()
