c75eb5ba4b2c73feca033307174f0982b591d7f1
George Steil <steilga92@gmail.com>
2025-01-02 09:57:39 -0600

    Added groups and two for loops to iterate over objects in the groups to main.py

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(x = (SCREEN_WIDTH / 2), y = (SCREEN_HEIGHT / 2))
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return	
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		for object in updatable:
+			object.update(dt)
+			
+		for object in drawable:
+			object.draw(screen)
+		
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 	
